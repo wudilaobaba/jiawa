179c9dc149e7a2b027b7ca1b263d6eefd7169e1e
whj <1273803935@qq.com>
2019-09-21 17:20:26 +0800

    da

--- jdbc01/src/main/java/cn/ttten/DBUtils3.java
+++ jdbc01/src/main/java/cn/ttten/DBUtils3.java
@@ -1,16 +1,38 @@
 package cn.ttten;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class DBUtils3 {
+	private static String driver;
+	private static String url;
+	private static String username;
+	private static String password;
+	static{
+		Properties prop = new Properties();
+		//得到文件的输入流
+		InputStream ips = DBUtils3.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		try {
+			prop.load(ips);
+			driver = prop.getProperty("driver");
+			url = prop.getProperty("url");
+			username = prop.getProperty("username");
+			password = prop.getProperty("password");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	//封装获取连接对象的代码
-	public static Connection getConn() throws Exception {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db3", "root", "whjlyn938751");
+	public static Connection getConn() throws Exception {	
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, username, password);
 		return conn;
 	}
 	
